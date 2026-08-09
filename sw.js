fd361c4427bb7d3f7ae80e5816ec6a58ae420d1a
shachnevan-tech <shachneva.n@gmail.com>
2026-08-09 13:29:43 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='kbju-v3';
+const CACHE_NAME='kbju-v4';
 const urlsToCache=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
