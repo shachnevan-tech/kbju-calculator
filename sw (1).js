35def09ef952cdf47bc76302d25d4a345bbc67f5
shachnevan-tech <shachneva.n@gmail.com>
2026-08-09 19:11:07 +0300

    Delete sw (1).js

--- sw (1).js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME='kbju-v4';
-const urlsToCache=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
